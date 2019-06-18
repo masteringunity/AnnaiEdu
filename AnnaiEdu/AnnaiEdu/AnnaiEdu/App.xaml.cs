84332a4dbb0f6ab8ceb84f1802a355edce78e7b5
jebaseelan <jebaseelan10@gmail.com>
2019-06-18 22:13:42 +0530

    installed : Install-Package reactiveui-xamforms -Version 7.4.0

--- AnnaiEdu/AnnaiEdu/AnnaiEdu/App.xaml.cs
+++ AnnaiEdu/AnnaiEdu/AnnaiEdu/App.xaml.cs
@@ -11,7 +11,7 @@ namespace AnnaiEdu
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            //MainPage = new MainPage();
         }
 
         protected override void OnStart()
